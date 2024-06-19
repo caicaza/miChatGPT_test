f85bfc8b5e0d1cec0ab0971b1216b69042c97738
Digtester454 <141531017+Digtester454@users.noreply.github.com>
2024-06-19 15:12:32 -0500

    colocar un objeto three js

--- chatgpt2/src/app/threejs/threejs.component.ts
+++ chatgpt2/src/app/threejs/threejs.component.ts
@@ -1,6 +1,8 @@
 import { Component, AfterViewInit, Inject, HostListener } from '@angular/core';
 
 import * as THREE from 'three';
+import { ThreeScene } from './three-scene'; // Importa la clase ThreeScene desde el archivo separado
+
 
 @Component({
   selector: 'app-threejs',
@@ -11,68 +13,14 @@ import * as THREE from 'three';
 ]
 })
 export class ThreejsComponent implements AfterViewInit {
-  private camera!: THREE.PerspectiveCamera;
-  private renderer!: THREE.WebGLRenderer;
-  private scene!: THREE.Scene;
-  private container: HTMLDivElement | null = null;
+  private scene: ThreeScene;
 
-
-  constructor(@Inject(Window) private window: Window) {}
+  constructor(@Inject(Window) private window: Window) {
+    this.scene = new ThreeScene(this.window);
+  }
 
   ngAfterViewInit(): void {
-    // Use setTimeout to ensure Angular has finished rendering the view
-    setTimeout(() => {
-      this.container = this.window.document.getElementById('three-js-container') as HTMLDivElement;
-
-      if (this.container) {
-        const width = this.container.clientWidth;
-        const height = this.container.clientHeight;
-
-        this.camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 10);
-        this.camera.position.z = 1;
-
-        this.scene = new THREE.Scene();
-
-        const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-        const material = new THREE.MeshNormalMaterial();
-
-        const mesh = new THREE.Mesh(geometry, material);
-        this.scene.add(mesh);
-
-        this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-        this.renderer.setSize(width, height);
-        this.renderer.setClearColor(0x000000, 0); // Transparent background
-
-        if (this.container) {
-          this.container.appendChild(this.renderer.domElement);
-        }
-
-        this.renderer.setAnimationLoop(this.animate.bind(this));
-
-        this.onResize();
-      }
-    }, 0);
-  }
-
-  @HostListener('window:resize', ['$event'])
-  onResize() {
-    if (this.container) {
-      const width = this.container.clientWidth;
-      const height = this.container.clientHeight;
-
-      this.camera.aspect = width / height;
-      this.camera.updateProjectionMatrix();
-
-      this.renderer.setSize(width, height);
-    }
-  }
-
-  private animate(time: number) {
-    const mesh = this.scene.children[0] as THREE.Mesh;
-    mesh.rotation.x = time / 2000;
-    mesh.rotation.y = time / 1000;
-
-    this.renderer.render(this.scene, this.camera);
+    this.scene.init('three-js-container');
   }
 
 
